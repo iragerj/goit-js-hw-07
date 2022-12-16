b0f284df5e1b86c1271a056e21892c0fafd25849
iragerj <iragerj@gmail.com>
2022-12-16 18:20:43 +0200

    HW task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryWrapper = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryWrapper.addEventListener("click", onGalleryWrapperClick);
+
+galleryWrapper.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const modal = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map((galleryItem) => {
+      return `<a class="gallery__item" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+    </a> `;
+    })
+    .join("");
+}
+
+function onGalleryWrapperClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  window.addEventListener("keydown", onModalclose);
+}
+
+function onModalclose(event) {
+  if (modal && event.keyCode === 27) {
+    modal.close();
+  }
+}
